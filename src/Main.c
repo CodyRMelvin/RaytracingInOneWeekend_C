76b3dd34dad8f041fb4913967bc2c9ff40e2f2fc
CodyRMelvin <codyrmelvin@gmail.com>
2025-12-27 23:04:32 -0800

    Completed chapter 2, tweaked cmake structure so that terminal is printed to while running

--- src/Main.c
+++ src/Main.c
@@ -2,6 +2,31 @@
 
 int main(void)
 {
-    printf("Hi mom!\n");
+    FILE* ppm = fopen( "image.ppm", "rw+" );
+    
+    int imageWidth = 256, imageHeight = 256;
+
+    fprintf( ppm, "P3\n%i %i\n255\n", imageWidth, imageHeight );
+
+    for ( int j = 0; j < imageHeight; ++j )
+    {
+        printf( "\rScanlines remaining: %i ", imageHeight - j );
+        fflush(stdout);
+
+        for ( int i = 0; i < imageWidth; ++i )
+        {
+            double r = (double)i / ( imageWidth - 1 );
+            double g = (double)j / ( imageHeight - 1 );
+            double b = 0.0;
+
+            int ir = (int)( 255.999 * r );
+            int ig = (int)( 255.999 * g );
+            int ib = (int)( 255.999 * b );
+
+            fprintf( ppm, " %i %i %i\n", ir, ig, ib );
+        }
+    }
+    
+    fclose(ppm);
     return 0;
 }
